115465619bbfd56de07b58bad3c7c648c3bf9275
FinalNumber <cjskkkk@naver.com>
2024-10-21 14:35:34 +0900

    세팅

---
+++ src/main/java/com/example/iot_project_backserver/BackServer.java
@@ -0,0 +1,12 @@
+package com.example.iot_project_backserver;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackServer {
+    public static void main(String[] args) {
+        SpringApplication.run(BackServer.class, args);
+    }
+}
---
+++ src/test/java/com/example/iot_project_backserver/IotProjectBackServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.iot_project_backserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IotProjectBackServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
