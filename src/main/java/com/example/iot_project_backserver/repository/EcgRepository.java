3ca195b4b0f6f5adc9b8d49aa58e06c0a655e28d
FinalNumber <cjskkkk@naver.com>
2024-11-25 01:01:16 +0900

    집데id 여부에 따른 ì어ë업데이트 구현 전

--- src/main/java/com/example/iot_project_backserver/repository/EcgRepository.java
+++ src/main/java/com/example/iot_project_backserver/repository/EcgRepository.java
@@ -3,8 +3,12 @@ package com.example.iot_project_backserver.repository;
 import com.example.iot_project_backserver.entity.ECG;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EcgRepository extends JpaRepository<ECG, Long>, HealthDataRepository<ECG>{
     boolean existsByUserId(String userId);
+
+    //Optional<ECG> findByUserId(String userId);
 }
 
 
