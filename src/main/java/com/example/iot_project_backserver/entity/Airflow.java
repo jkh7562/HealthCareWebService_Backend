e55e70a0f0c2b76655606e9935de78de0c08d76a
FinalNumber <cjskkkk@naver.com>
2024-11-25 15:49:35 +0900

    add entity

--- src/main/java/com/example/iot_project_backserver/entity/Airflow.java
+++ src/main/java/com/example/iot_project_backserver/entity/Airflow.java
@@ -17,6 +17,8 @@ public class Airflow {
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키 생성
     private Long id; //TODO 사용자 ID를 기본키로 사용하려면 수정 필요
 
+    //임시 확인용
+
     private String userId;// 사용자 ID
     @ElementCollection // 리스트 형태로 측정 데이터를 저장
     private List<Float> airflowdata; // 측정 데이터
